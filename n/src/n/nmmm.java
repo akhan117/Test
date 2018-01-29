20214e107288128cbb829fe39f73fe6cc0b71c9f
akhan117 <35905084+akhan117@users.noreply.github.com>
2018-01-28 19:23:16 -0500

    Add files via upload

---
+++ n/src/n/nmmm.java
@@ -0,0 +1,28 @@
+package n;
+
+
+
+public class nmmm {
+	public static void main(String[] args) {
+		
+		int a=9/2;
+		System.out.println(a);
+		int sss[]= {1,2,3,4,5,6,6};
+		int w[]=new int[sss.length];
+		for(int i=0;i<sss.length;i++) {
+			w[i]=sss[i];
+		}
+		System.out.println(w);
+		System.out.println(w.length);
+		
+		
+	}
+	public static void printStuff() {
+		int a=9/2;
+		System.out.println(a);
+		
+		}
+		
+		
+	}
+
